482daf8c9ef0283f05526e2b3e8012584f2904db
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2020-11-24 22:30:07 -0500

    Create BankAccountEntity

---
+++ 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/Migrations/20201125031105_BankAccountEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Oracle.EntityFrameworkCore.Metadata;
+
+namespace VolvoCash.DistributedServices.MainContext.Migrations
+{
+    public partial class BankAccountEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    LastModifiedAt = table.Column<DateTime>(nullable: true),
+                    Account = table.Column<string>(maxLength: 20, nullable: false),
+                    CCI = table.Column<string>(maxLength: 40, nullable: true),
+                    IsDefault = table.Column<bool>(nullable: false),
+                    BankId = table.Column<int>(nullable: false),
+                    DealerId = table.Column<int>(nullable: true),
+                    ArchiveAt = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccount", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccount_Banks_BankId",
+                        column: x => x.BankId,
+                        principalTable: "Banks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BankAccount_Dealers_DealerId",
+                        column: x => x.DealerId,
+                        principalTable: "Dealers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccount_BankId",
+                table: "BankAccount",
+                column: "BankId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccount_DealerId",
+                table: "BankAccount",
+                column: "DealerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccount");
+        }
+    }
+}
