0ef26dd4ffa35578bdfb1f8a9b46493bd34ec629
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2020-11-26 02:21:23 -0500

    Add migration for required CCI to Bank Account

---
+++ 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/Migrations/20201126071642_CCIRequiredForBankAccount.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VolvoCash.DistributedServices.MainContext.Migrations
+{
+    public partial class CCIRequiredForBankAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CCI",
+                table: "BankAccounts",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(40)",
+                oldMaxLength: 40,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CCI",
+                table: "BankAccounts",
+                type: "NVARCHAR2(40)",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+    }
+}
