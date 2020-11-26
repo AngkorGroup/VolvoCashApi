60ce7cce12eb31039539e950e99e0db97beaa9de
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2020-11-26 01:02:26 -0500

    Create migrations for add company and dealer bank account to liquidations

---
+++ 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/Migrations/20201126055227_CompanyAndDealerBankAccountToLiquidation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VolvoCash.DistributedServices.MainContext.Migrations
+{
+    public partial class CompanyAndDealerBankAccountToLiquidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyBankAccount",
+                table: "Liquidations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DealerBankAccount",
+                table: "Liquidations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyBankAccount",
+                table: "Liquidations");
+
+            migrationBuilder.DropColumn(
+                name: "DealerBankAccount",
+                table: "Liquidations");
+        }
+    }
+}
