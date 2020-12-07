34b302856774c3a3be804cf6af79d0ed4385413b
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2020-12-07 02:31:12 -0500

    Add Unsigned Contacts Report

--- 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/ApiWeb/Requests/Reports/ClientsCardUseReportRequest.cs
+++ 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/ApiWeb/Requests/Reports/ClientsCardUseReportRequest.cs
@@ -2,11 +2,9 @@ using System.Collections.Generic;
 
 namespace VolvoCash.DistributedServices.MainContext.ApiWeb.Requests.Reports
 {
-    public class ClientsCardUseReportRequest
+    public class ClientsCardUseReportRequest : ReportRequest
     {
         #region Properties
-        public string Type { get; set; }
-
         public List<string> CardTypes { get; set; }
 
         public string ClientId { get; set; }
--- 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/ApiWeb/Requests/Reports/UnsignedContactsReportRequest.cs
+++ 2. Distributed Services/2.2. MainContext/VolvoCash.DistributedServices.MainContext/ApiWeb/Requests/Reports/UnsignedContactsReportRequest.cs
@@ -2,16 +2,14 @@ using System.Collections.Generic;
 
 namespace VolvoCash.DistributedServices.MainContext.ApiWeb.Requests.Reports
 {
-    public class ChargesRankingReportRequest : ReportRequest
+    public class UnsignedContactsReportRequest : ReportRequest
     {
         #region Properties
         public string StartDate { get; set; }
 
         public string EndDate { get; set; }
 
-        public List<string> CardTypes { get; set; }
-
-        public bool EconomicGroup { get; set; }
+        public List<string> Clients { get; set; }
         #endregion
     }
 }
